1f63ab9ba93183ab49c59a9eced4995f12063a7c
username <2806257319@qq.com>
2020-04-14 15:21:09 +0800

    test(测试)：测试的编写 1:本地测试已经完成。

---
+++ src/main/java/com/ckf/test/Test.java
@@ -0,0 +1,14 @@
+package com.ckf.test;
+
+/**
+ * @author 安详的苦丁茶
+ * @version 1.0
+ * @date 2020/4/14 15:15
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+
+}
