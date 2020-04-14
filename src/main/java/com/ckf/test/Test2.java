5a1a1c3db4500f659895a8b452193f1551355f67
username <2806257319@qq.com>
2020-04-14 17:09:39 +0800

    deco(第二次提交)  编写了欢迎页

---
+++ src/main/java/com/ckf/test/Test2.java
@@ -0,0 +1,12 @@
+package com.ckf.test;
+
+/**
+ * @author 安详的苦丁茶
+ * @version 1.0
+ * @date 2020/4/14 17:06
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("hello ssh");
+    }
+}
